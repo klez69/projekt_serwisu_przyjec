a4de1be3d566bbb07e655379896e71e242ecdb81
klez69 <118447126+klez69@users.noreply.github.com>
2024-05-29 09:06:36 +0200

    poprawa

--- potPrzyjecia/list.php
+++ potPrzyjecia/list.php
@@ -1,36 +1,39 @@
 <?php
 session_start();
-include ('include/dtbas.php');
+include('include/dtbas.php');
 
-if (!isset($_SESSION['s']) || $_SESSION['s']!==true) {
-	if ( isset($_GET['s']) & ($_GET['s']=='$klucz') ) {
-		$_SESSION['s'] = true;
-	} else {
-		$_SESSION['s'] = false;
-	}
+if (!isset($_SESSION['s']) || $_SESSION['s'] !== true) {
+    if (isset($_GET['s']) && $_GET['s'] === '$klucz') {
+        $_SESSION['s'] = true;
+    } else {
+        $_SESSION['s'] = false;
+    }
 }
 
-if ( isset($_GET['l']) & ($_GET['l']==1) ) {
-	$_SESSION['s'] = false;
+if (isset($_GET['l']) && $_GET['l'] == 1) {
+    $_SESSION['s'] = false;
 }
 
-if ($_SESSION['s'] == true) {
-	
-	include ('layout/header.php');
+if ($_SESSION['s'] === true) {
 
-	include ('include/dtbas.php');
+    include('layout/header.php');
+    include('include/dtbas.php');
 
+    // Tworzenie połączenia
+    $db = new mysqli($db_srvr1, $db_user1, $db_pswd1, $db_dtbs1);
 
-		
-	$db = @mysql_connect ($db_srvr1, $db_user1, $db_pswd1) or die ($db_error01);
-	mysql_select_db($db_dtbs1, $db) or die ($db_error02);
+    // Sprawdzanie połączenia
+    if ($db->connect_error) {
+        die("Connection failed: " . $db->connect_error);
+    }
 
-	include ('include/list.php');
+    include('include/list.php');
 
-	mysql_close($db);
-	include ('layout/footer.php');
+    // Zamknięcie połączenia
+    $db->close();
+    include('layout/footer.php');
 } else {
-	header("Location: http://serwis.notebookservice.pl/");
+    header("Location: http://[strona]");
+    exit();
 }
-
 ?>
