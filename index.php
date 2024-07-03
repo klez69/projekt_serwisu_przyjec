a25d77a07c813edc112c9c7ddc7156b7597168d1
klez69 <118447126+klez69@users.noreply.github.com>
2024-07-03 11:12:19 +0200

    pop

--- index.php
+++ index.php
@@ -1,3 +1,37 @@
-<?
+<!DOCTYPE html>
+<html lang="pl">
 
-?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PRZYJECIA</title>
+
+    <!-- Linki do stylów CSS -->
+    <link rel="stylesheet" type="text/css" href="potPrzyjecia/css/style.css" />
+    <link rel="stylesheet" type="text/css" href="potPrzyjecia/css/print.css" media="print" />
+
+    <!-- Linki do skryptów JavaScript -->
+    <script src="https://code.jquery.com/jquery-latest.js"></script>
+    <script src="potPrzyjecia/js/script.js" type="text/javascript"></script>
+    <script src="potPrzyjecia/js/ukryj.js" type="text/javascript"></script>
+</head>
+<body>
+    <header>
+        <h1>[NAZWA FIRMY]</h1>
+    </header>
+
+    <main>
+        <center>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio accusamus, ipsum nulla at exercitationem
+                commodi obcaecati quidem sit! Dolorem labore perferendis voluptates, neque dignissimos commodi optio
+                ipsum
+                possimus harum itaque.</p>
+        </center>
+
+        <?php
+        require_once('list.php');
+        ?>
+    </main>
+</body>
+
+</html>
